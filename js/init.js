68d4469314057679db250de58688d7d109ff1f4d
Howard Kam <tc@unk-426d04f9.adelphiacom.net>
2016-08-03 05:26:14 -1000

    First commit

---
+++ js/init.js
@@ -0,0 +1,15 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+ $('.carousel.carousel-slider').carousel({full_width: true});
+ 
+   $(document).ready(function(){
+    $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+  });
